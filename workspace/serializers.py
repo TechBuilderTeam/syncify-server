ff50d688571c15b38f100255d8721baf192bf4ec
sifath islam <sifathislam790@gmail.com>
2024-06-04 23:37:34 +0600

    [Fix] fix an error the api

--- workspace/serializers.py
+++ workspace/serializers.py
@@ -1,7 +1,9 @@
+import collections.abc
 from rest_framework import serializers
 from .models import *
 from accounts.serializers import UserRegisterSerializer
 from datetime import date
+import collections
 
 # * ================ This Serializer is for the WorkSpace Creation ================ * #
 class WorkSpaceSerializer(serializers.ModelSerializer):
@@ -122,19 +124,35 @@ class ScrumSerializer(serializers.ModelSerializer):
         model = Scrum
         fields = ['id', 'name', 'details','timeline_Name']
 
+# class CreateScrumSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Scrum
+#         fields = ['timeline_Name', 'name', 'details']
+
+#     def create(self, validated_data):
+#         timeline = validated_data.get('timeline_Name')
+#         if timeline and timeline.assign:
+#             validated_data['members'].set(timeline.assign)
+        
+#         return Scrum.objects.create(**validated_data)
+
+
 class CreateScrumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scrum
         fields = ['timeline_Name', 'name', 'details']
-
     def create(self, validated_data):
         timeline = validated_data.get('timeline_Name')
-        if timeline and timeline.assign:
-            validated_data['members'] = timeline.assign
-        
-        return Scrum.objects.create(**validated_data)
+        scrum = Scrum.objects.create(**validated_data) 
 
-
+        if timeline:
+            if hasattr(timeline,'assign'):
+                if isinstance(timeline.assign, collections.abc.Iterable):
+                    scrum.members.set(timeline.assign)
+                else:
+                    scrum.members.add(timeline.assign)
+        return scrum
+    
 # * ================ This Serializer is for the Task ================ * #
 # class TaskCreationSerializer(serializers.ModelSerializer):
 #     class Meta:
