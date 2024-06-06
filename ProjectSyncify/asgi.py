5b0b3f193b00e1ef62c2f882974de2632fb1d2f3
najmulislamnajim <ninbd17@gmail.com>
2024-06-06 22:54:07 +0600

    [FIX] model complexity

--- ProjectSyncify/asgi.py
+++ ProjectSyncify/asgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
-import chat.routing
+import chats.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectSyncify.settings')
 
@@ -9,6 +9,6 @@ application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket":URLRouter(
-            chat.routing.websocket_urlpatterns
+            chats.routing.websocket_urlpatterns
         ),
 })
